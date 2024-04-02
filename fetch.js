4307fec4b0cd69cb8fd5632fc0bb2acdf8e5627d
Pescaosabroso <victormr400@gmail.com>
2024-04-02 18:23:30 -0400

    a

---
+++ fetch.js
@@ -0,0 +1,27 @@
+const changer = document.getElementById("apiButton2");
+const img = document.getElementById("card")
+
+    function replaceImage() {
+    fetch('https://api.pokemontcg.io/v2/cards/swsh2-192')
+      .then(response => response.json())
+      .then(data => {
+      img.src = data.data.images.small;
+      })};
+   
+      
+changer.addEventListener("click", replaceImage);
+
+
+
+// const changer2 = document.getElementsById(apiButton2);
+
+// function replaceImage() {
+//     fetch('https://api.pokemontcg.io/v2/cards/bw1-18')
+//       .then(response => response.json())
+//       .then(data => {
+//         const cardImage = document.getElementById("card");
+//         cardImage.src = data.url;
+//       })};
+      
+      
+// changer2.addEventListener("click", replaceImage);
