000a0ca6814bc99f2b6a99dbc11640dcbaf38c99
Pescaosabroso <victormr400@gmail.com>
2024-04-12 23:51:47 -0400

    Last minute tweaking

--- fetch.js
+++ fetch.js
@@ -42,23 +42,56 @@ const hitPointsTitle = document.getElementById("ps");
     hitPointsTitle.style.display = "none";
     };
 
-// const skillType1 = document.getElementById();
-// const skill1 = document.getElementById();
-// const skill2 = document.getElementById();
-// const statsW = document.getElementById();
-// const statsR = document.getElementById();
-// const statsRc = document.getElementById();
-// const footNote = document.getElementById();
-// const footLogo = document.getElementById();
+const skill1 = document.getElementById("skilldesc1");
+const skillName1 = document.getElementById("skillname-1");
+const skillsIcon1 = document.getElementById("skills");
+  function changeSkill1() {
+    fetch('https://api.pokemontcg.io/v2/cards/swsh2-192')
+      .then (response => response.json())
+      .then (data => {
+      skillsIcon1.style.display = "none";
+      skillName1.style.display = "none";
+      skill1.textContent = data.data.rules;
+      })};
 
+const skill2 = document.getElementById("ability2");
+  function changeSkill2() {
+    fetch('https://api.pokemontcg.io/v2/cards/swsh2-192')
+      .then (response => response.json())
+      .then (data => {
+      skill2.style.display = "none";
+      })};
+
+const stats = document.getElementById("stats");
+  function changeStats() {
+    fetch('https://api.pokemontcg.io/v2/cards/swsh2-192')
+      .then (response => response.json())
+      .then (data => {
+      stats.style.display = "none";
+      })};
+
+const footNoteTitle = document.getElementById("stats-link");
+const footNote = document.getElementById("fn-text");
+const footLogo = document.getElementById("promo");
+  function changeFootNote() {
+    fetch('https://api.pokemontcg.io/v2/cards/swsh2-192')
+      .then (response => response.json())
+      .then (data => {
+      footNoteTitle.textContent = data.data.set.name;
+      footNoteTitle.href = "https://www.pokemon.com/us/pokemon-tcg/pokemon-cards/?swsh2=";
+      footNote.textContent = data.data.rarity;
+      footLogo.src = data.data.set.images.symbol;
+      })};
 
 changer.addEventListener("click", replaceImage);
 changer.addEventListener("click", changeName);
 changer.addEventListener("click", changeTier);
 changer.addEventListener("click", changeSubTier);
 changer.addEventListener("click", changeHp);
-
-
+changer.addEventListener("click", changeSkill1);
+changer.addEventListener("click", changeSkill2);
+changer.addEventListener("click", changeStats);
+changer.addEventListener("click", changeFootNote);
 
    
 changer2.addEventListener("click", replaceImage);
@@ -66,5 +99,9 @@ changer2.addEventListener("click", changeName);
 changer2.addEventListener("click", changeTier);
 changer2.addEventListener("click", changeSubTier);
 changer2.addEventListener("click", changeHp);
+changer2.addEventListener("click", changeSkill1);
+changer2.addEventListener("click", changeSkill2);
+changer2.addEventListener("click", changeStats);
+changer2.addEventListener("click", changeFootNote);
 
 
