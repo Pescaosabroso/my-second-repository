52d29da5d1ab3aae1268097899a7484d0a11534c
Pescaosabroso <victormr400@gmail.com>
2024-05-14 14:24:46 -0400

    New simplistic navbar

--- menus.js
+++ menus.js
@@ -1,6 +1,15 @@
-const hamMenu = document.querySelector(".hamburger-menu");
+// const hamMenu = document.querySelector(".hamburger-menu");
 
-const dropDownMenu = document.querySelector(".mobile-dropdown-menu");
+// const dropDownMenu = document.querySelector(".mobile-dropdown-menu");
+
+// hamMenu.addEventListener("click", () => {
+//   hamMenu.classList.toggle("active");
+//   dropDownMenu.classList.toggle("active");
+// });
+
+const hamMenu = document.querySelector(".ham-menu");
+
+const dropDownMenu = document.querySelector(".dropdown-menu");
 
 hamMenu.addEventListener("click", () => {
   hamMenu.classList.toggle("active");
